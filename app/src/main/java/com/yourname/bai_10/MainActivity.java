ec7f67b0dd3a943615619ee96e0e76eabe0e9273
LeThang24 <lenhatthang12a5@gmail.com>
2026-03-13 15:18:54 +0700

    Bai 10 máy tính đơn giản

---
+++ app/src/main/java/com/yourname/bai_10/MainActivity.java
@@ -0,0 +1,50 @@
+package com.yourname.bai_10;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtA, edtB;
+    Button btnAdd, btnSub, btnMul, btnDiv;
+    TextView tvResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtA = findViewById(R.id.edtA);
+        edtB = findViewById(R.id.edtB);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnSub = findViewById(R.id.btnSub);
+        btnMul = findViewById(R.id.btnMul);
+        btnDiv = findViewById(R.id.btnDiv);
+        tvResult = findViewById(R.id.tvResult);
+
+        btnAdd.setOnClickListener(v -> calculate("+"));
+        btnSub.setOnClickListener(v -> calculate("-"));
+        btnMul.setOnClickListener(v -> calculate("*"));
+        btnDiv.setOnClickListener(v -> calculate("/"));
+    }
+
+    private void calculate(String operator){
+        double a = Double.parseDouble(edtA.getText().toString());
+        double b = Double.parseDouble(edtB.getText().toString());
+        double result = 0;
+
+        switch (operator){
+            case "+": result = a + b; break;
+            case "-": result = a - b; break;
+            case "*": result = a * b; break;
+            case "/": result = a / b; break;
+        }
+
+        tvResult.setText("Kết quả: " + result);
+    }
+}
